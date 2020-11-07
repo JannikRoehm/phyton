72d9917784f1efcda69715d4ddb57a3c529c0046
wAx1809 <74106024+wAx1809@users.noreply.github.com>
2020-11-07 16:18:02 +0100

    passt die Meldung für Mofafahrer an
    wir wollten das ein wenig drastischer formulieren.

--- src/FPK/Session 2/Session_2_Aufgabe_6.py
+++ src/FPK/Session 2/Session_2_Aufgabe_6.py
@@ -4,7 +4,7 @@ print("Der Junge ist", alter)
 if alter < 14:
     print("Du darfst noch nicht Mofa fahren")
 elif alter >= 14 and alter < 16:
-    print("Du darfst Mofa fahren aber nicht Moped")
+    print("Du darfst Mofa fahren aber nicht Moped!!")
 elif alter >= 16 and alter < 18:
     print("Du darfst Moped fahren aber kein Auto")
 else:
